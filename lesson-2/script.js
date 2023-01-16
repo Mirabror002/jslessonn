0a064c969246dd879601a5f3c6db724b65b67cdc
Mirabror Rashidov [FE-17] <122270982+Mirabror002@users.noreply.github.com>
2023-01-16 17:53:41 +0500

    Add files via upload

---
+++ lesson-2/script.js
@@ -0,0 +1,10 @@
+let namePrompt = prompt("Ismingiz nma?")
+let surnamePrompt = prompt("Familyangiz nma?")
+let agePrompt = prompt("Yoshingiz nichda?")
+
+let user={} 
+user.name="Mirabror"
+user.surname="Rashidov"
+user.age="20"
+console.log(user)
+ 
